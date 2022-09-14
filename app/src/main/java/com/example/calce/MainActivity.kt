150e45dc26fcdc0edfb71f88824e10d14a4fe2c7
matth <matthias.zwicknagl@outlook.de>
2022-09-14 10:21:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calce/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.calce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        MainFragment()
+
+    }
+}
